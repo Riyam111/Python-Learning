2e419a7abd091d8a2d5f9b7727eefbe7455c379d
Riyam111 <riyam1680@gmail.com>
2025-09-08 20:26:19 +0530

    broadcasting done

---
+++ Numpy/brodcasting.py
@@ -0,0 +1,21 @@
+# you can easily calculate any thing without using for loop
+import numpy as np
+prices=np.array([100,200,300])
+discount=10
+final_prices=prices-(prices*discount/100)
+print(final_prices)
+
+# multyplying in whole array
+arr=np.array([100,200,300])
+res=arr*2
+print(res)
+
+#adding some value in 2d array
+arr_2d=np.array([[1,2,3],[4,5,6]])
+vector=np.array([10,20,30])
+res=arr_2d+vector
+ans=arr_2d[0]+vector
+print(res)
+print(ans)
+
+
